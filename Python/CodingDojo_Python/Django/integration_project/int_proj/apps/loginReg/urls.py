f50aa9b030a9a26ba52c7222d5421b03d12a2d0b
Sam <sam@Samuels-MacBook-Pro.local>
2017-02-23 23:57:35 -0800

    finsihed dojo secrets (I think)

--- Python/CodingDojo_Python/Django/integration_project/int_proj/apps/loginReg/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.index, name='my_index'),
-    url(r'^success/(?P<id>\d+)$', views.success, name='my_success'),
-    url(r'^register$', views.register, name='my_register'),
-    url(r'^login$', views.login, name='my_login')
-]
